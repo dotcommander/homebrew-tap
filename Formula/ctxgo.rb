f7e1db797cd6dcbfb8ceab7b530701bdd390c68d
Gary Blankenship <gary@blankenship.me>
2026-09-22 17:04:10 -0400

    chore(ctxgo): update formula to 1.2.0

--- Formula/ctxgo.rb
+++ Formula/ctxgo.rb
@@ -2,10 +2,10 @@ class Ctxgo < Formula
   desc "Local-first searchable history index for coding-agent sessions"
   homepage "https://github.com/dotcommander/ctxgo"
   url "https://github.com/dotcommander/ctxgo.git",
-      tag:      "v1.1.0-go",
-      revision: "5ac899a70683d95fb66d4b1a5b6fd13dd8fe6b45",
+      tag:      "v1.2.0",
+      revision: "29a7486442eff1daf0348e3558fc728802d46300",
       using:    :git
-  version "1.1.0"
+  version "1.2.0"
   head "https://github.com/dotcommander/ctxgo.git", branch: "main"
 
   depends_on "go" => :build
@@ -16,6 +16,7 @@ class Ctxgo < Formula
   end
 
   test do
-    assert_match "Search local agent history", shell_output("#{bin}/ctxgo --help")
+    assert_match "Usage: ctxgo <command> [flags]", shell_output("#{bin}/ctxgo --help")
+    assert_match version.to_s, shell_output("#{bin}/ctxgo --version")
   end
 end
