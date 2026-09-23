8525e3df764a22fa9c7c9978f5ae3966922d2e6f
Gary Blankenship <gary@blankenship.me>
2026-09-23 11:56:11 -0400

    chore(jskill): update formula to 0.7.0

--- Formula/jskill.rb
+++ Formula/jskill.rb
@@ -3,10 +3,10 @@ class Jskill < Formula
   homepage "https://github.com/dotcommander/jskill"
   # Defer the credential until download so formula metadata never contains it.
   token_var = ENV.key?("HOMEBREW_JSKILL_GITHUB_TOKEN") ? "HOMEBREW_JSKILL_GITHUB_TOKEN" : "HOMEBREW_GITHUB_API_TOKEN"
-  url "https://codeload.github.com/dotcommander/jskill/legacy.tar.gz/3c098e2504ca45214a756913e945f524c0b4adbc",
+  url "https://codeload.github.com/dotcommander/jskill/legacy.tar.gz/54a07840940999b692b80582696b8941f04470ec",
       headers: ["Authorization: Bearer {{HOMEBREW_DEFERRED_ENV:#{token_var}}}"]
-  version "0.6.0"
-  sha256 "5055ce4dd61405a1a1a28ac1d7c43852368b786c7322c36cbe738b30507d4d50"
+  version "0.7.0"
+  sha256 "00bfe1a08296e17ed99fbf70303183450080cc9faf9e7c79c758dbe9f911f652"
   head "https://github.com/dotcommander/jskill.git", branch: "main"
   depends_on "go" => :build
 
